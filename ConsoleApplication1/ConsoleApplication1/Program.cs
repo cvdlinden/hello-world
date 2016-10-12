1cc823724972fe306776d28418b35acdbe358386
cvdlinden <cvdlinden@gmail.com>
2016-10-13 00:04:05 +0200

    Amende through Git

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Added through Git.");
             Console.ReadKey();
         }
     }
